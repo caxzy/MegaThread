b8e914aafdc722e8d02596ddf664d6c479fa74fb
caxzy#3907 <cxrdopvc@gmail.com>
2026-05-11 20:10:17 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export', // Konieczne dla GitHub Pages
+  basePath: '/MegaThread', // Nazwa Twojego repozytorium
+  images: {
+    unoptimized: true, // GitHub Pages nie wspiera optymalizacji obrazów Next.js
+  },
 };
 
 export default nextConfig;
